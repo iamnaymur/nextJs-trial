e3e337af7c37996575bc4f45868b664f9ddb5605
iamnaymur <nrtusher1@gmail.com>
2023-06-20 18:48:45 +0600

    manage products error

---
+++ src/app/dashboard/manage-products/ManageProduct.jsx
@@ -0,0 +1,28 @@
+import ManageSingleProduct from "./ManageSingleProduct";
+
+const ManageProduct = ({ products }) => {
+  return (
+    <div>
+      <table className="border-collapse w-full">
+        <thead>
+          <tr>
+            <th className="border border-slate-400">Title</th>
+            <th className="border border-slate-400">Price</th>
+            <th className="border border-slate-400">Update</th>
+            <th className="border border-slate-400">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <ManageSingleProduct
+              key={product.id}
+              product={product}
+            ></ManageSingleProduct>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ManageProduct;
--- src/app/dashboard/manage-products/page.js
+++ src/app/dashboard/manage-products/page.js
@@ -1,7 +1,19 @@
-import React from "react";
+import ManageProduct from "./ManageProduct";
+import getAllProducts from "@/utils/getAllProducts";
 
-const ManageProductPage = () => {
-  return <div>Manage product page.</div>;
+export const metadata = {
+  title: "Manage Products | Dashboard | Next hero",
+  description: "Next Hero",
+};
+const ManageProductsPage = () => {
+  const products = getAllProducts();
+
+  return (
+    <div className="w-full mt-10">
+      <h1 className="mb-5 text-2xl font-semibold">Manage All products here.</h1>
+      <ManageProduct products={products} />
+    </div>
+  );
 };
 
-export default ManageProductPage;
+export default ManageProductsPage;
